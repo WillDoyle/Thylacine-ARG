22f7aeb73c6b578fa9634f70d9591eef49951232
AWARD220 <alexward151@gmail.com>
2023-04-06 13:41:24 +1000

    fucking with da p5js, and added media tags, website is now workable, with no fuckarounds
    i messed with the dotmatrix a bit soz

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
 let dots = [];
 let gridSize = 40;
-let dotSize = 3;
+let dotSize = 10;
 let maxDistance = 100;
+
+
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
 
   canvas.parent('sketch-container');
   noStroke();
- fill(255, 0, 40, 255)
+ fill(255, 255, 255, 155)
 
   
   // Create the grid of dots
@@ -21,7 +23,13 @@ function setup() {
   }
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function draw() {
+  let gridSize = round(windowWidth/100);
+  let dotSize = round(windowHeight/100);
   clear();
    /*remove this to create insane effect*/
   
@@ -31,14 +39,14 @@ function draw() {
       // Calculate the angle between the cursor and the dot
       let angle = atan2(mouseY - dots[i].y, mouseX - dots[i].x);
       // Update the dot position based on the angle
-      dots[i].x += cos(angle) * 2;
-      dots[i].y += sin(angle) * 2;
+      dots[i].x += cos(angle) * 10;
+      dots[i].y += sin(angle) * 10;
     } else {
       // Return the dot to its original position
       let originalX = map(i % gridSize, 0, gridSize-1, dotSize, width-dotSize);
       let originalY = map(floor(i/gridSize), 0, gridSize-1, dotSize, height-dotSize);
-      dots[i].x += (originalX - dots[i].x) * 0.1;
-      dots[i].y += (originalY - dots[i].y) * 0.1;
+      dots[i].x += (originalX - dots[i].x + random(-10,10)) * 0.1;
+      dots[i].y += (originalY - dots[i].y + random(-10,10)) * 0.1;
     }
     ellipse(dots[i].x, dots[i].y, dotSize, dotSize);
   }
